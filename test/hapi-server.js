75beb020334e1136693ff2917ab80b9bb704fdbf
Alfio Zappala <azappala@azaptree.com>
2013-09-23 19:48:41 -0400

    refactored and added more logging

--- test/hapi-server.js
+++ test/hapi-server.js
@@ -49,6 +49,10 @@ function startServer(callback, autoStart, port) {
 	return hapiServer;
 }
 
+var loggingClient = require('runrightfast-logging-client')({
+	url : 'http://localhost:8000/log'
+});
+
 describe('Hapi Server', function() {
 	var server = undefined;
 
@@ -56,253 +60,182 @@ describe('Hapi Server', function() {
 		server = startServer(done);
 	});
 
-	after(function(done) {
+	after(function() {
 		server.stop(function() {
-			console.log('server has stopped');
-			done();
+			console.log('*** after Hapi Server - server has stopped');
 		});
 	});
 
 	it('log a valid event', function() {
-		var loggingClient = require('runrightfast-logging-client')({
-			url : 'http://localhost:8000/log'
-		});
-
 		var event = {
 			tags : [ 'info' ],
 			data : 'test : log a valid event'
 		};
 
 		loggingClient.log(event);
-		expect(loggingClient.eventCount).to.equal(1);
-		expect(loggingClient.invalidEventCount).to.equal(0);
 	});
 
-	describe('can auto-start', function() {
-		var server = undefined;
-
-		before(function() {
-			server = startServer(undefined, true, 8001);
-		});
-
-		after(function(done) {
-			server.stop(function() {
-				console.log('server has stopped');
-				done();
-			});
-		});
-
-		it('log a valid event', function() {
-			var loggingClient = require('runrightfast-logging-client')({
-				url : 'http://localhost:8001/log'
-			});
+	it('starting the server, when it is already started causes no harm', function(done) {
+		server.start(function() {
+			console.log('Server should already be started');
 
 			var event = {
 				tags : [ 'info' ],
-				data : 'test : log a valid event'
+				data : 'test : starting the server, when it is already started causes no harm'
 			};
 
 			loggingClient.log(event);
-			expect(loggingClient.eventCount).to.equal(1);
-			expect(loggingClient.invalidEventCount).to.equal(0);
+			done();
 		});
 
-		it('starting the server, when it is already started causes no harm', function(done) {
+	});
+
+	it('can be restarted', function(done) {
+		var event = {
+			tags : [ 'info' ],
+			data : 'test : can be restarted'
+		};
+
+		server.stop(function() {
 			server.start(function() {
-				console.log('Server should already be started');
-
-				var loggingClient = require('runrightfast-logging-client')({
-					url : 'http://localhost:8001/log'
-				});
-
-				var event = {
-					tags : [ 'info' ],
-					data : 'test : starting the server, when it is already started causes no harm'
-				};
-
 				loggingClient.log(event);
-				try {
-					expect(loggingClient.eventCount).to.equal(1);
-					expect(loggingClient.invalidEventCount).to.equal(0);
-				} catch (error) {
-					done(error);
-				}
-
+				console.log('logged event after restarting server : loggingClient.eventCount = ' + loggingClient.eventCount);
 				done();
 			});
-
 		});
+	});
 
-		it('can be restarted', function(done) {
-			var loggingClient = require('runrightfast-logging-client')({
-				url : 'http://localhost:8001/log'
-			});
+	it('if already stopped, then stopping again cause no harm', function(done) {
+		var server2 = startServer(undefined, true, 8002);
 
-			var event = {
-				tags : [ 'info' ],
-				data : 'test : can be restarted'
-			};
-
-			server.stop(function() {
-				server.start(function() {
-					loggingClient.log(event);
-					console.log('logged event after restarting server : loggingClient.eventCount = ' + loggingClient.eventCount);
-					expect(loggingClient.eventCount).to.equal(1);
-					expect(loggingClient.invalidEventCount).to.equal(0);
-
-					done();
-				});
-			});
+		server2.stop(function() {
+			server2.stop(done);
 		});
 
 	});
 
-	describe('can be stopped', function() {
-
-		it('if already stopped, then stopping again cause no harm', function(done) {
-			server = startServer(undefined, true, 8002);
-
-			setImmediate(function() {
-				server.stop(function() {
-					console.log('server is stopped');
-
-					server.stop(function() {
-						console.log('called stop again');
-						done();
-					});
-				});
-			});
-
-		});
-
-	});
-
-	describe('manifest can be specified in multiple ways', function() {
-
-		it('can be specified via a function', function(done) {
-			var manifest = function() {
-				return {
-					pack : {},
-					servers : [ {
-						port : 8003,
-						options : {
-							labels : [ 'api' ]
-						}
-					} ],
-					plugins : {
-						'lout' : {},
-						'furball' : {}
+	it('manifest can be specified via a function', function(done) {
+		var manifest = function() {
+			return {
+				pack : {},
+				servers : [ {
+					port : 8003,
+					options : {
+						labels : [ 'api' ]
 					}
-				};
+				} ],
+				plugins : {
+					'lout' : {},
+					'furball' : {}
+				}
 			};
+		};
 
-			var options = {
-				manifest : manifest,
-				autoStart : false
-			};
+		var options = {
+			manifest : manifest,
+			autoStart : false
+		};
 
-			var HapiServer = require('../index');
-			var hapiServer = new HapiServer(options);
+		var HapiServer = require('../index');
+		var hapiServer = new HapiServer(options);
 
-			var loggingClient = require('runrightfast-logging-client')({
-				url : 'http://localhost:8003/log'
-			});
+		var loggingClient = require('runrightfast-logging-client')({
+			url : 'http://localhost:8003/log'
+		});
 
-			var event = {
-				tags : [ 'info' ],
-				data : 'test : can be specified via a function'
-			};
+		var event = {
+			tags : [ 'info' ],
+			data : 'test : can be specified via a function'
+		};
 
-			hapiServer.start(function() {
-				console.log('hapiServer.status = ' + hapiServer.status);
-				expect(hapiServer.status).to.equal('STARTED');
-				loggingClient.log(event);
-				hapiServer.stop();
-				done();
-			});
+		hapiServer.start(function() {
+			console.log('hapiServer.status = ' + hapiServer.status);
+			expect(hapiServer.status).to.equal('STARTED');
+			loggingClient.log(event);
+			hapiServer.stop(done);
+		});
+
+	});
+
+	it('manifest can be specified via a manifest supplied by a module', function(done) {
+		var options = {
+			manifest : '../test/manifest',
+			autoStart : false
+		};
+
+		var HapiServer = require('../index');
+		var hapiServer = new HapiServer(options);
+
+		hapiServer.start(function() {
+			hapiServer.stop(done);
 
 		});
 
-		it('can be specified via a manifest supplied by a module', function(done) {
-			var options = {
-				manifest : '../test/manifest',
-				autoStart : false
-			};
+	});
 
-			var HapiServer = require('../index');
-			var hapiServer = new HapiServer(options);
+	it('will not startup if the manifest is not a valid type', function(done) {
+		var options = {
+			manifest : 123,
+			autoStart : false
+		};
 
-			hapiServer.start(function() {
-				hapiServer.stop();
+		var HapiServer = require('../index');
+		try {
+			new HapiServer(options);
+			done(new Error('expected an Error because manifest is not a valid type'));
+		} catch (error) {
+			// expected
+			done();
+		}
+
+	});
+
+	it('will not startup if the manifest is not a valid', function(done) {
+		var options = {
+			manifest : {},
+			autoStart : false
+		};
+
+		var HapiServer = require('../index');
+		var server = new HapiServer(options);
+		server.start(function(err) {
+			if (err) {
+				console.error('server failed to startup as expected : ' + err.message);
 				done();
-			});
-
-		});
-
-		it('will not startup if the manifest is not a valid type', function(done) {
-			var options = {
-				manifest : 123,
-				autoStart : false
-			};
-
-			var HapiServer = require('../index');
-			try {
-				new HapiServer(options);
-				done(new Error('expected an Error because manifest is not a valid type'));
-			} catch (error) {
-				// expected
-				done();
+			} else {
+				done(new Error('expected an Error because manifest is not valid'));
 			}
-
 		});
 
-		it('will not startup if the manifest is not a valid', function(done) {
-			var options = {
-				manifest : {},
-				autoStart : false
-			};
+	});
 
-			var HapiServer = require('../index');
-			var server = new HapiServer(options);
-			server.start(function(err) {
-				if (err) {
-					console.error('server failed to startup as expected : ' + err.message);
-					done();
-				} else {
-					done(new Error('expected an Error because manifest is not valid'));
-				}
-			});
-
-		});
-
-		it('will not startup if the manifest is not a valid because plugin cannot be found', function(done) {
-			var options = {
-				manifest : {
-					pack : {},
-					servers : [ {
-						port : 8094,
-						options : {
-							labels : [ 'api' ]
-						}
-					} ],
-					plugins : {
-						'DOES_NOT_EXIST' : {}
+	it('will not startup if the manifest is not a valid because plugin cannot be found', function(done) {
+		var options = {
+			manifest : {
+				pack : {},
+				servers : [ {
+					port : 8094,
+					options : {
+						labels : [ 'api' ]
 					}
-				},
-				autoStart : false
-			};
-
-			var HapiServer = require('../index');
-			var server = new HapiServer(options);
-			server.start(function(err) {
-				if (err) {
-					console.error('server failed to startup as expected : ' + err.message);
-					done();
-				} else {
-					done(new Error('expected an Error because manifest is not valid'));
+				} ],
+				plugins : {
+					'DOES_NOT_EXIST' : {}
 				}
-			});
+			},
+			autoStart : false
+		};
 
+		var HapiServer = require('../index');
+		var server = new HapiServer(options);
+		server.start(function(err) {
+			if (err) {
+				console.error('server failed to startup as expected : ' + err.message);
+				done();
+			} else {
+				done(new Error('expected an Error because manifest is not valid'));
+			}
 		});
+
 	});
 });
